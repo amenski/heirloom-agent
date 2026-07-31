149bda4be9ffd2a5c2ea0974eadcaae731c6b31e
amenski <amantwd@gmail.com>
2026-07-31 07:27:40 +0200

    chore: delete orphaned UI modules and dead exports; roadmap bookkeeping

--- src/ui/core/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./prompt-buffer.js";
-export * from "./prompt-undo-redo.js";
-export * from "./slash-commands.js";
-export * from "./loading-text.js";
-export * from "./clipboard.js";
